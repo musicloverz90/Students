b008607a43a363e9c4c707445bee67a26aa92fa1
Saffi_Ahmed <88970335+musicloverz90@users.noreply.github.com>
2021-08-17 23:33:36 +0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<endl;
+	cout<<"Assalam o Alaikum!!!"<<endl;
+	cout<<"Welcome to the Programmer'szone console:"<<endl;
+	cout<<"How may i help you sir?"<<endl;
+	system("COLOR B");
+	system("pause");
+	cout<<"Ye to prank ho gya :)";
+	return 0;
+}
